77a68adfe55760facefb52156016766d80f878ca
Carlos Alvarado <calvarado@corpcxp.com>
2026-07-02 14:31:53 -0500

    main

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import { createVercelHandler } from '../backend/src/serverless';
-
-export default async function handler(request: unknown, response: unknown): Promise<void> {
-  const server = await createVercelHandler();
-  server(request as never, response as never);
-}
